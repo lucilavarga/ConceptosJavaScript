7d557cd10c8fab49f8d539de3e936dae70ae738c
lucilavarga <163354987+lucilavarga@users.noreply.github.com>
2024-04-16 21:21:07 -0500

    clase de 15/04/2024

--- Script/PracticasConceptos.js
+++ Script/PracticasConceptos.js
@@ -1,12 +1,44 @@
 //1. SIEMPRE SE DEBE HACER -> VERIFICAR QUE SI ESTE CONECTADO CON EL THML
 
-console.log("HOLA, SOY LA ACTIVIDAD DE HOY 10/04/2024");
+console.log("HOLA, SOY LA ACTIVIDAD DE HOY");
+f
 
+//2.TRAER LOS ELEMENTOS QUE NECESITA DE SU HTML -> los que tiene id ->los elementos que me traiga deben estar en una variable -> debe ser constante
 
-//2.TRAER LOS ELEMENTOS QUE NECESITA DE SU THML -> los que teiene id ->los elementos que me traiga deben estar en una variable -> debe ser constante
-
-//Document-> me accede a mi thml
+//Document-> me accede a mi html
 //getElementById-> me trae un elemento en particular por su identificador unico
 //dentro de los parentesisi DEBO PONER EL MISMO ID que puse en mi thml
-const edadInput = document.getElementById("edadInput");
+
+//Mi elemento 1 -> forma 1-> se llevan directamente a su js
+const edadInput = document.
+getElementById("edadInput");
 console.log(edadInput);
+
+//Mi elemento 2 -> una forma
+ //Const btn = document.getElementById("boton");
+getElementById("boton");
+//console.log(btn);
+
+const mensaje= document.getElementById("mensajeEdad");
+console.log(mensaje);
+
+//2da forma es mediante eventos y crean funciones asociadas
+function verificarEdad(){
+    //Yo quiero que cada que le de click al boton, me salga un mensaje de "hola"
+
+    console.log("Entro a mi funcion verificar edad");
+     //Para acceder no al elemento de thml si no a su valor,se debe escribir el . value
+    let edad = edadInput. value; 
+    console.log(edad);
+  //UTILIZAR CONDICIONALES  
+
+    if(edad>= 18){
+        //aca esto es lo que sucede si la condicion es verdadera
+        console.log("Eres mayor de edad")
+    } else{
+         //aca se define lo que sucede cuando la condicion
+         console.log("eres menor de edad") 
+    }
+
+}
+
--- Script/conceptosBasicos.js
+++ Script/conceptosBasicos.js
@@ -1,8 +1,9 @@
-Console.log ("HOLA,MUNDO!")
 // Esta es la forma de comentar en js
+
 // CONSOLE.LOG -> la forma de enviar mensajes a la consola del navegador
-console.log("HOLA DESARROLLADORES Y PSICOLOGOS")
-console.log ("mi") 
+console.log("HOLA DESARROLLADORES Y PSICOLOGOS");
+console.log ("mi mensaje");
+
 
 //CONCEPTOS BASICOS EN JAVASCRIPT
 
@@ -11,23 +12,22 @@ console.log ("mi")
 //3. Condicionales
 //4. Ciclos
 
-// LAS VARIABLES EN JS
+
+// LAS VARIABLES EN JS ->piensen en las variables como contenedores de informacion
 
 //Hay 3 tipos de variables en js
 
 // VAR -> un tipo de variable que ya no se usa (NO LA VAMOS A USAR) -> Cambiar en el tiempo
 
 // LET -> Es una variable que  cambia en el tiempo
-
-resultado =5+3
-resultado ="Luisa"
 //CONST -> una variable que no puede cambiar en el tiempo
 
 
 //TIPOS DE DATOS O INFORMACION QUE PODEMOS ALMACENAR
+//el = en JS no es comparativo, si no que sirve para asignar valores
 
 //Numericos
-let numeros= 7;
+sea  numero s= 7 ;
 
 // Cadena de caracteres -> texto -> string (en programacion)
 // El texto SIEMPRE debe estar dentro de comillas
@@ -38,7 +38,7 @@ let esVERDADERO = true; //TRUE -> VERDADERO
 let esFALSO = FALSE; //false -< falso
 
 //Arreglos -> arrays (en programacion)-> me permite almacenar varios datos al mismo tiempo en una misma variable
-// let arregloNombres =["Luisa","Emanuel","Dayannna"]
+// let arregloNombres =["Luisa","Emanuel","Dayannna"];
 let arregloNumeros = ["1","2","3","4"]
 
 //Objetos -> " una representacion de la vida real,pero en nuestro codigo"
@@ -51,35 +51,40 @@ let arregloNumeros = ["1","2","3","4"]
 // {}-> llaves -> para declarar objetos
 //[]-> corchetes -> son para declarar arreglos
 
-
-const celular ={
+const celular = {
     color:"blanco",
     modelo: "Galaxy A7",
     ancho:350,
     alto:800,
+    //El metodo -> un mensaje que me indica (representa) la accion
     enviarMensajes:function(){
         //entre estos corchetes al lado de unos parentesis yo establezco la accion a realizar
         console.log("Mensaje enviado");
     },
     //El metodo -> las acciones -> directamente una operacion
     sumar: function(){
-        console.log(5+4);
+        consola . iniciar sesion (5 + 4) ;
     }
-
 }
+
+
+//EJEMPLOS DE FUNCIONES
+
+//funciones -> acciones que ustedes pueden tener en su sitio web
+
 //Funciones -> pueden no recibir datos de entrada -> un procesamiento de datos y luego nos devuelve un resultado
 function buscarObjeto(){
     console.log("le muestra lo que esta buscando")
-
 }
-function filtrarInformacion(){
+
+function filtrarInformacion ( ) {
     console.log("me muetra solo informacion de la depresion");
 }
 
 
 //en la carpeta script van TODOS los archivos JS PERO destinados a darle dinamismo al sitio web NO SE RELACIONAN CON LA FUNCIONALIDADDEL SITIO A NIVEL DE BACKEND
 
-//NODE JS -> Utilizamos para poder implementar JS en nuestro icken -> operaciones logicas nos permite el FUNCIONAMIENTO de nuestro sitio web
+//NODE JS -> Utilizamos para poder implementar JS en nuestro backend -> operaciones logicas nos permite el FUNCIONAMIENTO de nuestro sitio web
 
 
 
